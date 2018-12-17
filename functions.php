65ae53238db475b07f85d0875df9a215cce139ee
codwerk <louis_widdy@live.fr>
2018-12-17 20:31:03 +0100

    liste articles, fonction dans le head, image a la une

--- functions.php
+++ functions.php
@@ -5,16 +5,16 @@
   *  fonction qui permet d'injecter notre feuille de style
   */
 
- define('LGMAC_VERSION', '1.0.2');
+ define('CW_VERSION', '1.0.3');
 
 //chargemment dans le front-end
 function cw_scripts() {
     //chargement des styles
-    wp_enqueue_style('cw_bootstrap-min-css', get_template_directory_uri() .'/css/bootstrap.min.css', array(), LGMAC_VERSION, 'all' );
-    wp_enqueue_style('cw_custom', get_template_directory_uri() .'/css/style.css', array('cw_bootstrap-min-css'), LGMAC_VERSION, 'all' );
+    wp_enqueue_style('cw_bootstrap-min-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), CW_VERSION, 'all' );
+    wp_enqueue_style('cw_custom', get_template_directory_uri() . '/style.css', array('cw_bootstrap-min-css'), CW_VERSION, 'all' );
 
     //chrgement des scripts
-    wp_enqueue_script('cw_script', get_template_directory_uri() .'/js/script.js', array('jquery'), LGMAC_VERSION, true );
+    wp_enqueue_script('cw_admin_script', get_template_directory_uri() . '/js/script.js', array('jquery'), CW_VERSION, true );
 }//fin function wc_scripts
 
 add_action('wp_enqueue_scripts', 'cw_scripts');
@@ -22,7 +22,20 @@ add_action('wp_enqueue_scripts', 'cw_scripts');
 // chargement dans l'admin
 function cw_admin_scripts(){
     //chargement des styles
-    wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri() .'/css/bootstrap.min.css', array(), LGMAC_VERSION, 'all' );
+    wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), CW_VERSION, 'all' );
 }//fin function cw_admin_scripts
 
 add_action('admin_init', 'cw_admin_scripts');
+
+
+/**
+ * Utilitaires
+ */
+
+function cw_setup(){
+
+  add_theme_support('post-thumbnails');
+
+}//fin function cw_setup
+
+add_action('after_setup_theme', 'cw_setup');
--- index.php
+++ index.php
@@ -1,17 +1,71 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html <?php language_attributes(); ?>>
 <head>
-  <meta charset="UTF-8">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor">
   <title>Mon site Test</title>
 
   <?php wp_head(); ?>
 
 </head>
 <body>
-  <h1>Salut c'est moi</h1>
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+  <div class="container">
+    <div class="jumbotron">
+      <h1>Salut c'est moi</h1>
+    </div>
   </div>
+
+  <section>
+    <div class="container">
+      <div class="row  m-dw-30">
+        <div class="col-sm-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/ms-icon.png" alt="" class="img-responsive">
+        </div>
+        <div class="col-sm-8">
+            <h1>Titre de l'article</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+
+      <div class="row m-dw-30">
+        <div class="col-sm-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/ms-icon.png" alt="" class="img-responsive">
+        </div>
+        <div class="col-sm-8">
+            <h1>Titre de l'article</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+
+      <div class="row m-dw-30">
+        <div class="col-sm-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/ms-icon.png" alt="" class="img-responsive">
+        </div>
+        <div class="col-sm-8">
+            <h1>Titre de l'article</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+
+      <div class="row m-dw-30">
+        <div class="col-sm-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/ms-icon.png" alt="" class="img-responsive">
+        </div>
+        <div class="col-sm-8">
+            <h1>Titre de l'article</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
 
     <?php wp_footer(); ?>
 </body>
