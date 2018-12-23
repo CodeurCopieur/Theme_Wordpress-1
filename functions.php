0d961021d4910450656a68c057d92b0c6ad9e1a2
codwerk <louis_widdy@live.fr>
2018-12-23 17:53:42 +0100

    chargement des script, enlever le generateur de versio enlever les guillemets à la francaise, support du titre

--- functions.php
+++ functions.php
@@ -13,8 +13,9 @@ function cw_scripts() {
     wp_enqueue_style('cw_bootstrap-min-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), CW_VERSION, 'all' );
     wp_enqueue_style('cw_custom', get_template_directory_uri() . '/style.css', array('cw_bootstrap-min-css'), CW_VERSION, 'all' );
 
-    //chrgement des scripts
-    wp_enqueue_script('cw_admin_script', get_template_directory_uri() . '/js/script.js', array('jquery'), CW_VERSION, true );
+    //chargement des scripts
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), CW_VERSION, true );
+    wp_enqueue_script('cw_admin_script', get_template_directory_uri() . '/js/script.js', array('jquery', 'bootstrap-js'), CW_VERSION, true );
 }//fin function wc_scripts
 
 add_action('wp_enqueue_scripts', 'cw_scripts');
@@ -33,8 +34,17 @@ add_action('admin_init', 'cw_admin_scripts');
  */
 
 function cw_setup(){
-
+  //support   des vignettes
   add_theme_support('post-thumbnails');
+
+  //enlève générateur de version
+  remove_action('wp_head', 'wp_generator');
+
+  //enlève les guillemets à la française
+  remove_filter('the_content', 'wptexturize');
+
+  //support du Titre
+  add_theme_support('title_tag');
 
 }//fin function cw_setup
 
